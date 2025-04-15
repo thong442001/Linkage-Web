6001c7d53af0dcfd534cefd89566f16f4c5131f8
thongdeptraiwa <thong442001@gmail.com>
2025-04-15 16:55:24 +0700

    15/4-reactions-store

--- src/navigations/WebNavigation.jsx
+++ src/navigations/WebNavigation.jsx
@@ -1,14 +1,17 @@
 // src/components/Login.js
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import HomeNavigation from './HomeNavigation';
 import UserNavigation from './UserNavigation';
+import {
+  getAllReaction,
+} from '../rtk/API';
+import { setReactions } from '../rtk/Reducer';
 const WebNavigation = () => {
+  const dispatch = useDispatch();
   const { user } = useSelector((state) => state.app);
 
   useEffect(() => {
-    // // check user có bị khóa ko
-    // callCheckBanUser();
     //reactions
     callGetAllReaction();
   }, []);
